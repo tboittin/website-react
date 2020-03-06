90770971407fc1f79cb62ca68fe7f7f2c68c867e
Thomas Boittin <tboittin@gmail.com>
2020-03-06 13:37:51 +0100

    Contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,10 +3,12 @@ import './Contact.scss'
 import ScrollToNext from './Navigation/ScrollToNext'
 import ScrollToPrevious from './Navigation/ScrollToPrevious'
 import AppFooter from './AppFooter';
+import { Col, Row } from 'react-bootstrap';
+import { FaLinkedinIn, FaEnvelope } from 'react-icons/fa';
 
 class Contact extends Component {
     render() {
-        const {sectionOrder, positionOrder} = this.props;
+        const {sectionOrder, positionOrder, infos} = this.props;
         return(
             <>
             <section id="contact">
@@ -15,6 +17,36 @@ class Contact extends Component {
                     positionOrder={positionOrder}
                 />
                 <div className="contactContent">
+                    <Row>
+                        <Col
+                            xs={6}
+                            className="text-center align-content-center"
+                        >
+                            <h3 className="p-5">LOCATION</h3>
+                            <h4>France</h4>
+                            
+                        </Col>
+                        <Col
+                            xs={6}
+                            className="text-center"
+                        >
+                            <h3 className="p-5">CONTACT</h3>
+                            <div className="contactIcon">
+                                <a href={infos.linkedIn} target="_blank">
+                                    <FaLinkedinIn 
+                                        size={35}
+                                        className="mx-3"
+                                    />
+                                </a>
+                                <a href={infos.mailto}>
+                                    <FaEnvelope 
+                                        size={35}
+                                        className="mx-3"
+                                    />
+                                </a>
+                            </div>
+                        </Col>
+                    </Row>
                     
                     
                 </div>
